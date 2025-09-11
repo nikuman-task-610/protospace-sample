249644a6d770933f87e1b4e8d34771dbac472641
nikuman-610 <yuuki.h110929@gmail.com>
2025-09-11 11:51:39 +0900

    Initial commit

---
+++ src/main/java/in/tech_camp/protospace_sample/ProtospaceSampleApplication.java
@@ -0,0 +1,13 @@
+package in.tech_camp.protospace_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtospaceSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtospaceSampleApplication.class, args);
+	}
+
+}
